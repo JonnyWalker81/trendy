17d58e059194daed9e6312d5f7cff12463c79bb5
Jonathan Rothberg <phantom@example.com>
2025-07-18 19:47:42 -0700

    Initial Commit

---
+++ trendyTests/trendyTests.swift
@@ -0,0 +1,17 @@
+//
+//  trendyTests.swift
+//  trendyTests
+//
+//  Created by Jonathan Rothberg on 7/18/25.
+//
+
+import Testing
+@testable import trendy
+
+struct trendyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
